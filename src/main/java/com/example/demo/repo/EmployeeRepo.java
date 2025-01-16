294b8622218ca260a879b08e03a1895ba4b5be1e
ahmed-tech-t <ahmedrashedengineer@gmail.com>
2025-01-16 03:44:01 +0200

    commit

--- src/main/java/com/example/demo/repo/EmployeeRepo.java
+++ src/main/java/com/example/demo/repo/EmployeeRepo.java
@@ -2,9 +2,11 @@ package com.example.demo.repo;
 
 import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-
+// name of end point
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
 
 }
